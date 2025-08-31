54b333913f076c5582ad22ba3539fd93ad335f8d
maheshgumma18 <maheshgumma18@gmail.com>
2025-09-01 04:09:38 +0530

    main

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -53,11 +53,15 @@ useEffect(() => {
    socket.on('onlineusers',(data)=>{
     console.log("the online users are",data)
          setOnline(data)
-   })
+   },
+     
+  )
     
     
     return () => {
       socket.off("receiveMessage");
+      socket.off("onlineusers");
+     
     };
   }, []);
 
